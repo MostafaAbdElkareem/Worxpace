3afc78c3e503833c9aeba4f6b80c3f874ea90d5c
mostafadisha <mostafa.abdelkareem@hotmail.com>
2019-10-15 22:56:01 +1100

    buidl folder

---
+++ build/precache-manifest.ac4670677feba58f345e7c73e2c5882e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3313c015849665e6fca2",
+    "url": "/static/js/main.3313c015.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "3313c015849665e6fca2",
+    "url": "/static/css/main.f9248162.chunk.css"
+  },
+  {
+    "revision": "9a19c903f778a3bd9a8b26742977f5ab",
+    "url": "/index.html"
+  }
+];
